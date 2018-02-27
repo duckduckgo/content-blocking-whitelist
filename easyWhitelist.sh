ad246eb0e2b41da9e710f2f4ad10f763c8b32a42
dharb <harbagio@gmail.com>
2018-02-27 08:01:15 -0800

    add '' to sed -i to make compatible with macOS

--- easyWhitelist.sh
+++ easyWhitelist.sh
@@ -2,7 +2,7 @@
 # may need to chmod 700 easyWhitelist.sh to run
 wget https://raw.githubusercontent.com/easylist/easylist/master/easylist/easylist_whitelist.txt && wget https://raw.githubusercontent.com/easylist/easylist/master/easyprivacy/easyprivacy_whitelist.txt
 # clear easylist whitelists
-sed -i '/!-----EasyPrivacy Whitelist Start-----!/,/!-----EasyList Whitelist End-----!/d' trackers-whitelist.txt
+sed -i'' '/!-----EasyPrivacy Whitelist Start-----!/,/!-----EasyList Whitelist End-----!/d' trackers-whitelist.txt
 # add easyprivacy whitelist
 echo '!-----EasyPrivacy Whitelist Start-----!' >> trackers-whitelist.txt
 sed -e 's/@@//' easyprivacy_whitelist.txt >> trackers-whitelist.txt
